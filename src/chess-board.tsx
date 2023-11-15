9c68cb42b6603a5b6f7384f5de9076a14e3d3fcb
MOnichshuk1 <monichshuk@dar.io>
2023-11-15 20:28:49 +0600

    updated react version

--- src/chess-board.tsx
+++ src/chess-board.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import singleSpaReact from "single-spa-react";
-import Root from "./root.component";
+import rootComponent from "./root.component";
 
-const lifecycles = singleSpaReact({
+export const { bootstrap, mount, unmount } = singleSpaReact({
   React,
-  ReactDOM,
-  rootComponent: Root,
+  ReactDOMClient,
+  rootComponent,
   errorBoundary(err, info, props) {
-    // Customize the root error boundary for your microfrontend here.
-    return null;
+    // https://reactjs.org/docs/error-boundaries.html
+    return <div>This renders when a catastrophic error occurs</div>;
   },
 });
-
-export const { bootstrap, mount, unmount } = lifecycles;
