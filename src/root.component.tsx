5e6e6b367956a63130c2c4c24206a419fa9ffcb3
MOnichshuk1 <monichshuk@dar.io>
2023-11-15 22:09:47 +0600

    added mui

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,21 @@
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@emotion/react";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export default function Root(props) {
-  return <section>dsnkfjkbdshfj is mounted!</section>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      sdsasdasd
+      <Button>dasd</Button>
+    </ThemeProvider>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "chess",
@@ -11,5 +18,79 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    // entry: "./src/root.component.tsx",
+    mode: "development",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: require.resolve("@svgr/webpack"),
+              options: {
+                prettier: false,
+                svgo: false,
+                svgoConfig: {
+                  plugins: [{ removeViewBox: false }],
+                },
+                titleProp: true,
+                ref: true,
+              },
+            },
+            {
+              loader: require.resolve("file-loader"),
+              options: {
+                name: "svg/[name].[hash].[ext]",
+              },
+            },
+          ],
+          issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "build.[contenthash].css",
+      }),
+      new webpack.ProvidePlugin({
+        React: "react",
+      }),
+      // new webpack.SourceMapDevToolPlugin({}),
+    ],
+    optimization: {
+      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    },
   });
 };
